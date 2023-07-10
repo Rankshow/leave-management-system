bb5cc17ef4bb9f5fadd6fd262ea82f0bfef46e73
Rankshow <uwaomaobinna20@gmail.com>
2023-07-10 12:06:22 +0100

    added addItem page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Component/Header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Component/Home';
 import About from './Component/About';
+import AddItem from './Component/AddItem';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
          <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/addItem' element={<AddItem />} />
          </Routes>
      </div>
     </>
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import { Table, Container, Col, Row } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { PlusCircleOutlined } from '@ant-design/icons';
-import {CloseCircleOutlined} from '@ant-design/icons';
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+// import { Table, Container, Col, Row } from 'react-bootstrap';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {CloseCircleOutlined} from '@ant-design/icons';
+// import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
 
 const Home = () => {
   
   return(
     <>
-    <div className='mt-[5em] flex items-center justify-between m-3'>
-       <h3 className='text-xl font-medium text-red-400'>Department</h3>
-        <button className='text-green-800 font-bold'><PlusCircleOutlined /></button>
+    <div className='max-w-2xl mx-auto mt-[5em] flex items-center justify-between mb-3'>
+       <h3 className='text-2xl font-medium text-red-400'>Department</h3>
+       <Link to="/addItem"><button className='text-green-700 rounded-md text-2xl tracking-wider hover:opacity-[.7]'><PlusCircleOutlined /></button></Link> 
     </div>
-    <div className='flex justify-center '>
+    <div className='flex justify-center max-w-4xl mx-auto'>
       <table className="table-auto w-screen">
-      <thead className="bg-blue-700 border-separate border-spacing-2 b text-white ">
+      <thead className="bg-blue-700 border-separate border-spacing-2 b text-white cursor-pointer ">
         <tr>
           <th className='border border-spacing-1 py-2'>Name</th>
           <th className='border border-spacing-1 py-2'>Procurement</th>
           <th className='border border-spacing-1 py-2'>Accounting</th>
         </tr>
       </thead>
-      <tbody>
-        <tr className='bg-gray-300'>
+      <tbody className='py-2'>
+        <tr className='bg-gray-300 text-center'>
           <td>The Sliding Mr. Bones </td>
           <td>Malcolm Lockyer</td>
           <td>1961</td>
         </tr>
-        <tr>
+        <tr className='text-center'>
           <td>Witchy Woman</td>
           <td>The Eagles</td>
           <td>1972</td>
         </tr>
-        <tr className='bg-gray-300'>
+        <tr className='bg-gray-300 text-center'>
           <td>Shining Star</td>
           <td>Earth, Wind, and Fire</td>
           <td>1975</td>
